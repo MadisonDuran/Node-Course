321380cb95e7a2445e329af12852b5fccd471076
Madison Duran <madisonduran74@gmail.com>
2025-08-24 16:27:24 -0400

    installed chalk and got some messages to load in my terminal.

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,11 +1,18 @@
-const validator = require('validator') 
+//const add = require('./utils.js')
+//const sum = add(4, -2)
+//console.log(sum)
+
+//const validator = require('validator')
+//console.log(validator.isURL(http/mead.io))
+
 const getNotes = require('./notes.js')
+import chalk from 'chalk';
 
 const msg = getNotes();
 console.log(msg)
 
-console.log(validator.isURL('http/mead.io'))
+const greenMsg = chalk.green.inverse.bold('Success!');
+console.log(greenMsg);
 
-// const add = require('./utils.js')
-// const sum = add(4, -2)
-// console.log(sum)
+
+
