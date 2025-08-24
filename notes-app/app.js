865ca108195e923a3a6106e79884505badf287a1
Madison Duran <madisonduran74@gmail.com>
2025-08-24 19:55:16 -0400

    Creating commands such as read, list, add, remove.

--- notes-app/app.js
+++ notes-app/app.js
@@ -23,6 +23,48 @@ if (command === 'add') {
     console.log('Removing note!')
 }
 
+const yargs = require('yargs')
+console.log(yargs.argv)
+
+// Customize yargs version
+yargs.version('1.1.0')
+
+// Create add command
+yargs.command({
+    command: 'add', 
+    describe: 'Add a new note',
+    handler: function () {
+        console.log('Adding a new note!')
+    }
+})
+
+// Create remove command
+.yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function () {
+        console.log('Remove the note')
+    }
+})
+
+// Create list command
+yargs.command({
+    command: 'list',
+    describe: 'List your notes',
+    handler: function () {
+        console.log('listing out all notes')
+    }
+})
+
+
+// Create read command
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: function () {
+        console.log('Reading a note')
+    }
+})
 
 
 
