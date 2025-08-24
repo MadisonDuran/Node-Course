342b6ee131d0b5a43fd349a8c112028e0a474432
Madison Duran <madisonduran74@gmail.com>
2025-08-24 15:08:49 -0400

    Created app.js, notes.js, added notes  to notes.txt, and added utils.js. installed the npm validator.

---
+++ notes-app/app.js
@@ -0,0 +1,9 @@
+const validator = require('validator') 
+
+const msg = getNotes();
+
+console.log(msg)
+
+// const add = require('./utils.js')
+// const sum = add(4, -2)
+// console.log(sum)
---
+++ notes-app/utils.js
@@ -0,0 +1,9 @@
+console.log('utils.js')
+
+const name = 'Madison'
+
+const sum = function (a, b) {
+    return a + b
+}
+
+export default sum
